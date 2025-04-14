54635f22d48c393b009d358f409a9615beff71c1
domingueroA <206634202+domingueroA@users.noreply.github.com>
2025-04-14 20:53:31 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/components/ContactUs/ContactUs.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './ContactUs.css';
+
+const ContactUs = () => {
+    return (
+        <div className="contact-us-container">
+            <h2>Contact Us</h2>
+            <p>Email: <a href="mailto:contact@vongee.com">contact@vongee.com</a></p>
+            <p>Phone: <a href="tel:+1-2818324261">+1-2818324261</a></p>
+        </div>
+    );
+};
+
+export default ContactUs;
